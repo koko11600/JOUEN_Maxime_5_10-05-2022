00cea74bc9b5b8524645a62311e4c47d2eabf665
maxime <maximejouen30@gmail.com>
2023-06-28 09:21:56 +0200

    Rajout du message de confirmation avant le passage Admin dans le HTML

--- views/viewUserManagement.php
+++ views/viewUserManagement.php
@@ -12,7 +12,7 @@
                             <h4 class="title"><a href="#"><b><?= $user['firstName'] .' '. $user['lastName'] ?> (<?= $user['role'] == 1 ? 'Admin' : 'User' ?>)</b></a></h4>
                             <ul class="post-footer">
                                 <?php if ($user['role'] == 0) { ?>
-                                    <li><a href="admin&modifyUser&id=<?= $user['id'] ?>" title="Passer en Admin"><i class="ion-android-person-add"></i></a></li>
+                                    <li><a href="admin&modifyUser&id=<?= $user['id'] ?>"onclick="return confirm('Voulez-vous ajoutés cette utilisateur Administrateur du site')" title="Passer en Admin"><i class="ion-android-person-add"></i></a></li>
                                 <?php } ?>
                                 <li><a href="admin&deleteUser&id=<?= $user['id'] ?>" onclick="return confirm('Voulez-vous supprimer définitivement ce compte ?')" title="Supprimer"><i class="ion-android-delete"></i></a></li>
                             </ul>
